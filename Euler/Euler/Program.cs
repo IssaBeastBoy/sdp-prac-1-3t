774208912ddda62762489de77d1482e5d6f250a9
Robin <gvgollie4@gmail.com>
2019-02-15 13:47:16 +0200

    Smallest multiple

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -10,6 +10,33 @@ namespace Euler
     {
         static void Main(string[] args)
         {
+            double i = 1; //The number to be compared
+            double num = 0; //A placeholder
+            bool uptotwnety = false; //Will check if it's divisable up to 20
+            while (true) //Infinte loop, breaks when number is found
+            {
+                for(int n = 1; n <21; n++) //Checks factors up to 20
+                {
+                    if (i % n == 0)
+                    {
+                        uptotwnety = true; //If it's divisable, it's true
+                    }
+                    else
+                    {
+                        uptotwnety = false; //If not, becomes fasle and jumps out of the loop
+                        break;
+                    }
+                }
+                if (uptotwnety) //If true at the end, the number is divisable by all up to 20
+                {
+                    num = i; //Assing it
+                    break; //Break the loop
+                }
+                i++;
+            }
+            Console.WriteLine($"{num}");
+            Console.ReadLine();
+
         }
     }
 }
